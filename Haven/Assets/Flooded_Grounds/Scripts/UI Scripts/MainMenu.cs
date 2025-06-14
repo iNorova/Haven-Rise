17de4721326b561b8531ed99e22a5b758d3624ee
Marcus Gaspar <marcusnikko28@gmail.com>
2025-06-14 15:20:25 +0800

    Main menu things

--- Haven/Assets/Flooded_Grounds/Scripts/UI Scripts/MainMenu.cs
+++ Haven/Assets/Flooded_Grounds/Scripts/UI Scripts/MainMenu.cs
@@ -3,15 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
     }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
 }
+
